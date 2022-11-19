22d707fb57930b1ae8625d6f0920beffab901c7b
Spydneo <jairoecalderon@gmail.com>
2022-11-19 20:49:04 +0000

    Add autoapi extension and test autoapi

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,14 @@ author = 'Jairo C.'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+# AUTOAPI
+extensions = [
+        'sphinx.ext.autodoc',
+        'autoapi.extension',
+]
+
+autoapi_type = 'python'
+autoapi_dirs = ['../../mipaquete']
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -23,5 +30,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'furo'
 html_static_path = ['_static']
