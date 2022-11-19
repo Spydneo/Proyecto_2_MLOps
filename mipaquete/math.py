ce3b677961c74302316d356e4ce6b62c43884a36
Spydneo <jairoecalderon@gmail.com>
2022-11-19 17:43:33 +0000

    Add testing

--- mipaquete/math.py
+++ mipaquete/math.py
@@ -3,9 +3,9 @@ def sumar(a, b):
     return a+b
 
 def restar(a, b):
-    return a+b
+    return a-b
 
 def multiplicar(a, b):
-    return a+b
+    return a*b
 
 
---
+++ tests/test_preprocessing.py
@@ -0,0 +1,17 @@
+import mipaquete.math as math
+# from mipaquete.math import sumar
+
+def test_math_sumar():
+    """Comprueba que la operación sumar sea correcta
+    """
+    assert math.sumar(3, 5) == 8
+
+def test_math_restar():
+    """Comprueba que la operación restar sea correcta
+    """
+    assert math.restar(7, 2) == 5
+
+def test_math_multiplicar():
+    """Comprueba que la operación multiplicar sea correcta
+    """
+    assert math.restar(7, 2) == 5
